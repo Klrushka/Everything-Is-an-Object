163894d2cd2589dc7274748264a4d7ac61d047c0
Klrushka <76685224+Klrushka@users.noreply.github.com>
2021-05-27 00:45:09 +0300

    Add files via upload

---
+++ EverythingIsAnObject/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Task 1
+        System.out.println("Int value: " + Task1.value1);
+        System.out.println("Char value: " + Task1.value2 + '\n');
+
+        // Task 2
+        HelloDate.HelloDate();
+        System.out.println('\n');
+
+        // Task 8
+        TestObject t1 = new TestObject();
+        TestObject t2 = new TestObject();
+        System.out.println("Static field t1: " + t1.getStaticField() + '\n' +
+                "Static field t2: " + t2.getStaticField() + '\n');
+
+        // Task 9
+        Character ch = 'x';
+        Integer i = 9;
+        Double d = 3d;
+
+        /*
+        Task 16 in class Tree16
+         */
+
+
+
+    }
+}
---
+++ EverythingIsAnObject/src/Task1.java
@@ -0,0 +1,4 @@
+public class Task1 {
+    static int value1;
+    static char value2;
+}
