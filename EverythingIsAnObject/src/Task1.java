524c09147cc62d17a6601c0d23b90b397adc496d
Klrushka <76685224+Klrushka@users.noreply.github.com>
2021-06-07 11:59:31 +0300

    Add files via upload

---
+++ EverythingIsAnObject/src/Task1.java
@@ -0,0 +1,4 @@
+public class Task1 {
+    int value1;
+    char value2;
+}
---
+++ EverythingIsAnObject/src/TestObject.java
@@ -0,0 +1,7 @@
+public class TestObject {
+    static String staticField = "Hello, I'm static field";
+
+    public String getStaticField() {
+        return staticField;
+    }
+}
